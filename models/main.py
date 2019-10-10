81756d0de81242a363aacf95cc82c2ab0b9e5ac8
gabriel <gabosoft.ape@gmail.com>
2019-10-10 03:55:04 -0500

    update syntax of the model

--- models/main.py
+++ models/main.py
@@ -8,6 +8,6 @@ from odoo import models, fields, api, _
 
 
 class PropertyNext(models.Model):
-    _name = 'property.liabilities_features'
+    _name = 'liabilities.features'
 
     name = fields.Char("Nombre")
