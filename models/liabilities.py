8b7a5e219e2f292173f17c8edef1349ce60a4773
gabriel <gabosoft.ape@gmail.com>
2019-10-10 05:18:17 -0500

    update model

--- models/liabilities.py
+++ models/liabilities.py
@@ -7,8 +7,38 @@
 from odoo import models, fields, api, _
 
 
-class PropertyNext(models.Model):
+class LiabilitiesFeatures(models.Model):
     _name = 'liabilities.features'
 
     name = fields.Char("Nombre")
     cod = fields.Float(string="Codigo")
+
+class LiabilitiesAsambleas(models.Model):
+    _name = 'liabilities.asambleas'
+    _description = "Asambleas"
+    _rec_name = 'nombre'
+
+    land_id = fields.Many2one('property.company', string='Conjunto')
+    doc_count = fields.Integer(string="Numero de documentos", compute='_get_attached_docs')
+    nombre = fields.Char('Nombre de la asamblea')
+
+    @api.multi
+    def _get_attached_docs(self):
+        for record in self:
+            domain = [('res_model', '=', self._name), ('res_id', '=', record.id)]
+            record.doc_count = self.env['ir.attachment'].search_count(domain)
+
+    @api.multi
+    def attachment_tree_view(self):
+        domain = [('res_model', '=', self._name), ('res_id', 'in', self.ids)]
+        return {
+            'name': 'Documentos',
+            'domain': domain,
+            'res_model': 'ir.attachment',
+            'type': 'ir.actions.act_window',
+            'view_id': False,
+            'view_mode': 'kanban,tree,form',
+            'view_type': 'form',
+            'limit': 80,
+            'context': "{'default_res_model': '%s','default_res_id': %d}" % (self._name, self.id)
+        }
