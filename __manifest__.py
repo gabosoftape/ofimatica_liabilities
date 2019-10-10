a3c77c4eaf84b698cd3b02f927352776def0439b
gabriel <gabosoft.ape@gmail.com>
2019-10-10 03:19:36 -0500

    update menu vieww

--- __manifest__.py
+++ __manifest__.py
@@ -5,7 +5,7 @@
 
 {
     "name": "Obligaciones Ofimatica",
-    'version': '12.0.1.2.0',
+    'version': '12.0.2.2.0',
     "author": "Gabriel Pabón",
     "website": "https://www.ofimaticasoluciones.com.co",
 
